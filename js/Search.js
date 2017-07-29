9ac9ea9bc47149d946236a21beffdd319ee6dd97
UHanczar <u.hanczar@gmail.com>
2017-07-29 15:57:54 +0300

    f_b

--- js/Search.js
+++ js/Search.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import ShowCard from './ShowCard';
 import preload from './../data.json';
 
-const
+class Search extends Component {
+
+  state = {
+    searchTerm: ''
+  }
+
+  handleSearchTermChange = event => {
+    this.setState({searchTerm: event.target.value});
+  }
+
+  render(){
+    return (
+      <div className='search'>
+        <header>
+          <h1>svideo</h1>
+        <input
+          onChange={this.handleSearchTermChange} value={this.state.searchTerm}
+          type='text'
+          placeholder='Search'
+        />
+        </header>
+        <div>
+          {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+      </div>
+    );
+  }
+};
 export default Search;
