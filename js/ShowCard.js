2e880ca441a8f6b3100c6a824146583e6d5ff719
UHanczar <u.hanczar@gmail.com>
2017-07-30 02:13:02 +0300

    Configuring_flow

--- js/ShowCard.js
+++ js/ShowCard.js
@@ -1,3 +1,5 @@
+// @flow
+
 import React from 'react';
 import {string } from 'prop-types';
 import styled from 'styled-components';
@@ -17,7 +19,12 @@ const Image = styled.img`
   margin-right:10px;
 `;
 
-const ShowCard = (props) => (
+const ShowCard = (props: {
+  poster: string,
+  title: string,
+  year: string,
+  description: string
+}) => (
   <Wrapper>
     <Image src={`/public/img/posters/${props.poster}`} alt={`${props.title} Show Poster`} />
     <div>
@@ -27,12 +34,5 @@ const ShowCard = (props) => (
     </div>
   </Wrapper>
 );
-
-ShowCard.propTypes = {
-  poster: string.isRequired,
-  title: string.isRequired,
-  year: string.isRequired,
-  description: string.isRequired
-};
 
 export default ShowCard;
