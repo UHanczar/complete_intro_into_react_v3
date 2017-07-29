3ec741aa18253d47eaea9c44e17d7bb4180dc268
UHanczar <u.hanczar@gmail.com>
2017-07-29 22:37:51 +0300

    testing_components_with_enzyme

--- js/__tests__/Search.test.js
+++ js/__tests__/Search.test.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-
+import { shallow } from 'enzyme';
+// import renderer from 'react-test-renderer';
+// your can't use renderer with enzyme
+// enzyme tests only aim component. If you want to go deeper use {render, static}
 import Search from './../Search';
+import ShowCard from'./../ShowCard';
+import preload from './../../data.json';
 
 test('Search renders correctly', () => {
-  const component = renderer.create(<Search />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  const component = shallow(<Search />);
+  expect(component).toMatchSnapshot();
+});
+
+test('Search should render correct amount shows', () => {
+  const component = shallow(<Search />);
+  expect(preload.shows.length).toEqual(component.find(ShowCard).length);
+});
+
+test('Search should render the correct amount of shows based on search term', () => {
+  const searchWord = 'black';
+  const component = shallow(<Search />);
+
+  component.find('input').simulate('change', { target: {value: searchWord} });
+  const showCount = preload.shows.filter(show => `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0).length;
+
+  expect(component.find(ShowCard).length).toEqual(showCount);
 });
