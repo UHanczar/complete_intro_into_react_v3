a5b8109e85e219ebab0630941705a98412e858e6
UHanczar <u.hanczar@gmail.com>
2017-07-30 13:21:41 +0300

    Enhancing_routing_and_creating_details_component

--- js/App.js
+++ js/App.js
@@ -2,9 +2,13 @@
 
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import type { Match } from 'react-router-dom';
+
+import preload from './../data.json';
 
 import Landing from './Landing';
 import Search from './Search';
+import Details from './Details';
 import FourOhFour from './PageNotFound';
 
 const App = () => (
@@ -12,7 +16,11 @@ const App = () => (
     <div className='app'>
       <Switch>
         <Route exact path='/' component={Landing} />
-        <Route path='/search' component={Search} />
+        <Route path='/search' component={props => <Search shows={preload.shows} {...props}/>} />
+        <Route path='/details/:id' component={(props: { match: Match }) => {
+          const selectedShow = preload.shows.find(show => props.match.params.id === show.imdbID);
+          return <Details show={selectedShow} {...props} />;
+        }} />
         <Route component={FourOhFour} />
       </Switch>
     </div>
