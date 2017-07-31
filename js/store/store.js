de51363751dbff2142c9e7c3d0a47db7804fe5ec
UHanczar <u.hanczar@gmail.com>
2017-08-01 00:57:45 +0300

    Using_combineReducers

--- js/store/store.js
+++ js/store/store.js
@@ -1,7 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 
 import rootReducer from './../reducers/reducers';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, compose(typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f));
 
 export default store;
