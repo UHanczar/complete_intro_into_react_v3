34d5c9dff63ea07dedc71021e45315ba1164f0db
UHanczar <u.hanczar@gmail.com>
2017-08-02 23:09:35 +0300

    Setting_async_routing

--- js/App.js
+++ js/App.js
@@ -8,21 +8,35 @@ import { Provider } from 'react-redux';
 import preload from './../data.json';
 
 import store from './store/store';
-import Landing from './components/Landing';
-import Search from './components/Search';
-import Details from './components/Details';
+import AsyncRoute from './components/AsyncRoute';
+// import Landing from './components/Landing';
+// import Search from './components/Search';
+// import Details from './components/Details';
 import FourOhFour from './components/PageNotFound';
 
 const App = () => (
   <Provider store={store}>
     <div className='app'>
       <Switch>
-        <Route exact path='/' component={Landing} />
-        <Route path='/search' component={props => <Search shows={preload.shows} {...props}/>} />
-        <Route path='/details/:id' component={(props: { match: Match }) => {
-          const selectedShow = preload.shows.find(show => props.match.params.id === show.imdbID);
-          return <Details show={selectedShow} {...props} />;
-        }} />
+        <Route exact path="/" component={props => <AsyncRoute props={props} loadingPromise={import('./components/Landing')} />} />
+        <Route
+          path="/search"
+          component={props => (
+            <AsyncRoute loadingPromise={import('./components/Search')} props={Object.assign({ shows: preload.shows }, props)} />
+          )}
+        />
+        <Route
+          path="/details/:id"
+          component={(props: { match: Match }) => {
+            const selectedShow = preload.shows.find((show: Show) => props.match.params.id === show.imdbID);
+            return (
+              <AsyncRoute
+                loadingPromise={import('./components/Details')}
+                props={Object.assign({ show: selectedShow, match: {} }, props)}
+              />
+            );
+          }}
+        />
         <Route component={FourOhFour} />
       </Switch>
     </div>
